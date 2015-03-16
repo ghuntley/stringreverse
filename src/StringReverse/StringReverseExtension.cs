aa1c9ac52ba2e015d0ed5e405a1261de7c536586
Geoffrey Huntley <ghuntley@ghuntley.com>
2015-03-16 17:06:14 +1100

    initial commit :rocket:

---
+++ src/StringReverse/StringReverseExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace StringReverse
+{
+    public static class StringReverseExtension
+    {
+        public static string Reverse(this string value)
+        {
+            if (String.IsNullOrWhiteSpace(value))
+            {
+                return value;
+            }
+
+            var enumerator = StringInfo.GetTextElementEnumerator(value);
+            var elements = new List<string>();
+
+            while (enumerator.MoveNext())
+            {
+                elements.Add(enumerator.GetTextElement());
+            }
+
+            elements.Reverse();
+
+            return String.Concat(elements);
+        }
+    }
+}
